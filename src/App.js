4fdd62ed7a6c330813caba2e441e7701d6972208
Miguel Ángel Durán García <midudev@Mac-mini-de-Miguel.local>
2019-07-08 23:37:22 +0200

    Download image functionality and add footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import { h } from 'preact'
-import {useState} from 'preact/hooks'
+import {useRef, useState} from 'preact/hooks'
+
+import Link from './Link'
 
 export default function App () {
+    const ref = useRef()
     const [text, setText] = useState("HEY! 🙋‍♀️ MAKE YOUR OWN SPEECH BUBBLE!")
+    
+    function downloadImage () {
+        Promise.all([
+            import('html-to-image'),
+            import('downloadjs')
+        ]).then(([{toPng}, download]) => {
+            toPng(ref.current).then(dataUrl => download(dataUrl, 'komic.png'))
+        })
+    }
+
     return (
+      <main>
         <section>
-            <div>
-                <div class="bubble" contenteditable>{text}</div>
+          <div ref={ref}>
+            <div className="bubble" contentEditable>
+              <span>WRITE HERE YOUR MESSAGE</span>
+              <span id="cursor">|</span>
             </div>
-            <br /><br />
-            <button onClick={() => {}}><span>Save!</span></button>
+          </div>
         </section>
+        <section>
+          <button onClick={downloadImage}><span>Save Img!</span></button>
+        </section>
+        <footer>
+          <p>based on the mythical <Link href='http://wigflip.com/ds/' external>wigflip tool</Link></p>
+        </footer>
+      </main>
     )
 }
