424f93a37d782f8d6dc38d372e9931d5fea93d56
midudev <miduga@gmail.com>
2020-01-25 15:48:26 +0100

    Create Link component to reuse

--- src/Link.js
+++
@@ -1,6 +0,0 @@
-import { h } from 'preact'
-
-export default function ({children, href, external}) {
-    const rel = external ? 'noopener nofollow' : undefined
-    return <a href={href} rel={rel}>{children}</a>
-}
