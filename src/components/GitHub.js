6f4695c61d6b4848910acd440317c35ae3b52c0a
midudev <miduga@gmail.com>
2020-01-25 15:48:14 +0100

    Add Github Component

---
+++ src/components/GitHub.js
@@ -0,0 +1,9 @@
+import {h} from 'preact'
+
+export default () => (
+  <div className="github-wrapper">
+    <Link href="https://github.com/midudev/komic.app" external>
+      <i className="pixel-icon github" />
+    </Link>
+  </div>
+)
