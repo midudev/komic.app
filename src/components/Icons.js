c855e66418e979e35bc0184f97ee0dc2629fe1f6
midudev <miduga@gmail.com>
2020-02-09 18:36:15 +0100

    Some fixes

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -3,3 +3,7 @@ import { h } from 'preact'
 export const FloopyIcon = () => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><path d="M-130.255 14.799h43.972v1.626h-43.972zM-130.255 9.413h43.972v1.619h-43.972zM0 100h100V0H0v100zM53.335 6.671v13.326H6.671V6.671h46.664zm0 86.658h-6.67V73.332h6.67v19.997zm13.334-33.323h-6.671v6.656H40.002v-6.656h-6.671V39.994h6.671v-6.656h19.996v6.656h6.671v20.012zm6.663-26.668h-6.663v-6.67h6.663v6.67z" /></svg>
 )
+
+export const SettingsIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill='#222' viewBox="0 0 12 15"><path d="M9 8v1H7v1h2v1h2V9h-1V8h1V7H9v1zM5 4h2v1H5zM9 3v2h2V4h-1V3h1V1H9v1H7v1h2zM3 9V7H1v1h1v1H1v2h2v-1h2V9H3zM5 7h2v1H5zM5 10h2v2H5zM5 0h2v2H5zM4 5h1v2H4zM0 5h1v2H0zM11 5h1v2h-1zM3 5V3h2V2H3V1H1v2h1v1H1v1h2zM7 5h1v2H7z" /></svg>
+)
--- src/components/Link.js
+++ src/components/Link.js
@@ -2,5 +2,5 @@ import { h } from 'preact'
 
 export default function ({children, href, external}) {
     const rel = external ? 'noopener nofollow' : undefined
-    return <a href={href} rel={rel}>{children}</a>
+    return <a href={href} rel={rel} target={external ? '_blank' : ''}>{children}</a>
 }
